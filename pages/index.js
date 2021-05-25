8a8987dd114d7d010fa65ca174ea992ff105c8ea
earllee333 <jerrykiet2012@gmail.com>
2021-05-25 17:51:42 +0800

    pp

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import 'semantic-ui-css/semantic.min.css'
 
 export default function Home() {
   return (
