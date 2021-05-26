c532f1a19905d9f286179223268a7a869c22cb16
earllee333 <jerrykiet2012@gmail.com>
2021-05-27 02:27:58 +0800

    ccv

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,22 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const GA_TRACKING_ID ='G-R05RRVHFCQ'
   return (
     <>
       <Head >
         <title>台中1992 Shisha</title>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-R05RRVHFCQ"></script>
-      <script src='./script.js'></script>
+      <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `
+            }}
+          />
       </Head>                                       
       <div className='home page'>
         <h1>1992 Shisha</h1>
---
+++ utils/analytics.js
@@ -0,0 +1,21 @@
+import ReactGa from 'react-ga'
+
+export const initGA=()=>{
+    console.log('Ga init')
+    ReactGa.initialize('G-R05RRVHFCQ')
+}
+export const logPageView = () => {
+    console.log(`Logging pageview for${window.location.pathname}`)
+    ReactGa.set({page:window.location.pathname})
+    ReactGa.pageview(window.location.pathname)
+}
+export const logEvent = (category="",action="")=>{
+    if(category&&action){
+        ReactGa.event({category,action})
+    }
+}
+export const logException =(description="",fatal=false)=>{
+    if(decription){
+        ReactGa.exception({description,fatal})
+    }
+}
